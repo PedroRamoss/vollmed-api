1acac26352fdcfac0dff5f118d44d728e7a4b559
Pedro Henrique <33028556+PedroRamoss@users.noreply.github.com>
2025-07-25 14:38:34 -0300

    Create script-deploy.sh

---
+++ script-deploy.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+docker load -i vollmed-api.tar
+
+mv docker-compose-prod.yaml docker-compose.yaml
+
+container_ids=$(docker ps -q)
+
+if [ -z "$container_ids" ]; then
+  echo "não há containers em execução"
+else
+  for container_id in $container_ids; do
+    echo "Parando container: $container_id"
+    docker stop $container_id
+  done
+  echo "Todos os containers foram parados"
+fi
+docker compose up -d
